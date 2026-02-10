d2988a9201aa1321fb89863481ff059eb7bf6e91
pandeyGanesh <pandey.ganesh12@gmail.com>
2026-02-10 20:29:52 +0530

    Initial commit

---
+++ src/main/java/com/tripplings/dukaan/DukaanApplication.java
@@ -0,0 +1,13 @@
+package com.tripplings.dukaan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DukaanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DukaanApplication.class, args);
+	}
+
+}
