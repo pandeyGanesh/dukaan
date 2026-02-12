9a38ad8909bcd089c1f23f75ae550202e653acaf
pandeyGanesh <pandey.ganesh12@gmail.com>
2026-02-12 23:28:31 +0530

    3. Create roles entity

---
+++ src/main/java/com/tripplings/dukaan/roles/Role.java
@@ -0,0 +1,22 @@
+package com.tripplings.dukaan.roles;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "roles")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private UUID id;
+    private String authority;
+}
